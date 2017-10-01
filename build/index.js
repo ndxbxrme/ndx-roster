001b7d061d5d84c81390a4c3402bf959d08aafa4
ndxbxrme <lewis_the_cat@hotmail.com>
2017-10-01 09:02:34 +0100

    npm

--- build/index.js
+++ build/index.js
@@ -1,6 +1,45 @@
 (function() {
   'use strict';
-  module.exports = function(ndx) {};
+  var async;
+
+  async = require('async');
+
+  module.exports = function(ndx) {
+    var doRoster;
+    ndx.roster = {
+      filter: function(users, cb) {
+        return cb(users);
+      }
+    };
+    doRoster = function(socket) {
+      return ndx.socket.users(function(users) {
+        return ndx.roster.filter(users, function(users) {
+          return async.each(users, function(user, callback) {
+            var myusers;
+            myusers = [];
+            Object.assign(myusers, users);
+            if (ndx.permissions) {
+              return ndx.permissions.check('select', {
+                table: 'users',
+                user: user,
+                objs: myusers
+              }, ndx.permissions.dbPermissions(), function() {
+                ndx.socket.emitToUsers([user], 'roster', myusers);
+                return callback();
+              });
+            } else {
+              ndx.socket.emitToUsers([user], 'roster', myusers);
+              return callback();
+            }
+          });
+        });
+      });
+    };
+    if (ndx.socket) {
+      ndx.socket.on('user', doRoster);
+      return ndx.socket.on('disconnect', doRoster);
+    }
+  };
 
 }).call(this);
 
--- src/index.coffee
+++ src/index.coffee
@@ -1,3 +1,26 @@
 'use strict'
-
+async = require 'async'
 module.exports = (ndx) ->
+  ndx.roster =
+    filter: (users, cb) ->
+      cb users
+  doRoster = (socket) ->
+    ndx.socket.users (users) ->
+      ndx.roster.filter users, (users) ->
+        async.each users, (user, callback) ->
+          myusers = []
+          Object.assign myusers, users
+          if ndx.permissions
+            ndx.permissions.check 'select',
+              table: 'users'
+              user: user
+              objs: myusers
+            , ndx.permissions.dbPermissions(), ->
+              ndx.socket.emitToUsers [user], 'roster', myusers
+              callback()
+          else
+            ndx.socket.emitToUsers [user], 'roster', myusers
+            callback()
+  if ndx.socket
+    ndx.socket.on 'user', doRoster
+    ndx.socket.on 'disconnect', doRoster
