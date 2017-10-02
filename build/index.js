822c2cbdc8fed47dac0f6aead433da06c84c1b4a
ndxbxrme <lewis_the_cat@hotmail.com>
2017-10-02 22:59:58 +0100

    socket upgrade

--- build/index.js
+++ build/index.js
@@ -12,28 +12,30 @@
       }
     };
     doRoster = function(socket) {
-      return ndx.socket.users(function(users) {
-        return ndx.roster.filter(users, function(users) {
-          return async.each(users, function(user, callback) {
-            var myusers;
-            myusers = [];
-            Object.assign(myusers, users);
-            if (ndx.permissions) {
-              return ndx.permissions.check('select', {
-                table: 'users',
-                user: user,
-                objs: myusers
-              }, ndx.permissions.dbPermissions(), function() {
+      if (socket.user) {
+        return ndx.socket.users(function(users) {
+          return ndx.roster.filter(users, function(users) {
+            return async.each(users, function(user, callback) {
+              var myusers;
+              myusers = [];
+              Object.assign(myusers, users);
+              if (ndx.permissions) {
+                return ndx.permissions.check('select', {
+                  table: 'users',
+                  user: user,
+                  objs: myusers
+                }, ndx.permissions.dbPermissions(), function() {
+                  ndx.socket.emitToUsers([user], 'roster', myusers);
+                  return callback();
+                });
+              } else {
                 ndx.socket.emitToUsers([user], 'roster', myusers);
                 return callback();
-              });
-            } else {
-              ndx.socket.emitToUsers([user], 'roster', myusers);
-              return callback();
-            }
+              }
+            });
           });
         });
-      });
+      }
     };
     if (ndx.socket) {
       ndx.socket.on('user', doRoster);
--- src/index.coffee
+++ src/index.coffee
@@ -5,22 +5,23 @@ module.exports = (ndx) ->
     filter: (users, cb) ->
       cb users
   doRoster = (socket) ->
-    ndx.socket.users (users) ->
-      ndx.roster.filter users, (users) ->
-        async.each users, (user, callback) ->
-          myusers = []
-          Object.assign myusers, users
-          if ndx.permissions
-            ndx.permissions.check 'select',
-              table: 'users'
-              user: user
-              objs: myusers
-            , ndx.permissions.dbPermissions(), ->
+    if socket.user
+      ndx.socket.users (users) ->
+        ndx.roster.filter users, (users) ->
+          async.each users, (user, callback) ->
+            myusers = []
+            Object.assign myusers, users
+            if ndx.permissions
+              ndx.permissions.check 'select',
+                table: 'users'
+                user: user
+                objs: myusers
+              , ndx.permissions.dbPermissions(), ->
+                ndx.socket.emitToUsers [user], 'roster', myusers
+                callback()
+            else
               ndx.socket.emitToUsers [user], 'roster', myusers
               callback()
-          else
-            ndx.socket.emitToUsers [user], 'roster', myusers
-            callback()
   if ndx.socket
     ndx.socket.on 'user', doRoster
     ndx.socket.on 'disconnect', doRoster
